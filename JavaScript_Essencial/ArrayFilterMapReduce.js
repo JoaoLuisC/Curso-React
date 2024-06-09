729b6567d4d31e46d51f4d7ae0700c9116f31584
Joao luis cardoso <joao.lc12@htomail.com>
2024-06-09 18:18:38 -0300

    Curso React, Section 21, aula 410

---
+++ JavaScript_Essencial/ArrayFilterMapReduce.js
@@ -0,0 +1,11 @@
+//Retorne a soma do dobro de todos os pares
+
+const numeros = [ 5, 80, 50, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+
+const questao = numeros.filter(value => value % 2 === 0).
+map(value => value*2).reduce((acc,value) => acc + value);
+
+console.log(questao);
+//filer retorna : [ 80, 50, 2, 8, 22 ]
+//map retorna: [ 160, 100, 4, 16, 44 ]
+//reduce retorna: 324
